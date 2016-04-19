076a91a269ed2ba07686b5c4b30712dd6c534245
lyshia <alyshiabentley@gmail.com>
2016-04-18 17:15:54 -0700

    addd week 02 objects and get numbers

---
+++ Week02-ObjectBasicsJasmine/Source/MyObject.js
@@ -0,0 +1,20 @@
+var myObject = {
+    numberOne: 1,
+    numberTwo: 2,
+    stringOne: 'string',
+
+    methodOne: function() {
+        'use strict';
+        return 1;
+    },
+    methodTwo: function() {
+        'use strict';
+        return 2;
+    },
+    methodThree: function() {
+        'use strict';
+        return 3;
+    }
+};
+
+module.exports = myObject;
