5f93403ebf359be261a772e2183fbee5ff30e86c
lyshia <alyshiabentley@gmail.com>
2016-05-09 15:17:41 -0700

    add expressroutessolar

--- Week05-ExpressRoutesSolar/public/javascripts/control.js
+++ Week05-ExpressRoutesSolar/public/javascripts/control.js
@@ -1,2 +1,21 @@
-$(document).ready(function() { 'use strict'; 
+$(document).ready(function () {
+    'use strict';
+
+    $('#renewables').click(doThis);
+
+    function doThis(){
+    $.getJSON('/renewables', function (response) {
+            console.log(response);
+        })
+        .done(function () {
+            console.log("second success");
+        })
+        .fail(function (a, b, c) {
+            console.log('Error', a, b, c);
+            $('#debug').html('Error occured: ', a.status);
+        })
+        .always(function () {
+            console.log("complete");
+        });
+      }
 });
