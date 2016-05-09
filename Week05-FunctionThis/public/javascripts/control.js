6dc3b6976b098c045abe8f442033b6455749c002
lyshia <alyshiabentley@gmail.com>
2016-05-09 01:47:25 -0700

    finish funciton this

--- Week05-FunctionThis/public/javascripts/control.js
+++ Week05-FunctionThis/public/javascripts/control.js
@@ -6,9 +6,37 @@ function getThis() {
     return this;
 }
 
-var getThisAnonymous = function(){
+var getThisAnonymous = function() {
     return this;
 }
 
-$(document).ready(function() { 'use strict';
+$(document).ready(function() {
+    'use strict';
 });
+
+function getThisStrict() {
+    'use strict';
+    return;
+}
+
+var MyFunction = function() {};
+
+MyFunction.prototype.getThis = function() {
+    return this;
+}
+
+var myFunction = new MyFunction();
+
+myFunction.getThis = function() {
+    return myFunction;
+}
+
+var myObject = function() {
+    this.getThis = function() {
+        return myObject;
+    }
+}
+
+myObject.getThis = function() {
+    return myObject;
+}
