2fd1f39a07750923fd6039cca59c371f67664348
Charlie on RohanElf <charlie@elvenware.com>
2016-05-29 10:42:44 -0700

    Charlie cleaning up unmerged main.js

--- Week05-ExpressRoutesSolar/public/javascripts/main.js
+++ Week05-ExpressRoutesSolar/public/javascripts/main.js
@@ -8,23 +8,14 @@ requirejs.config({
     paths: {
         jquery: 'components/jquery/dist/jquery',
         bootstrap: 'components/bootstrap/dist/js/bootstrap',
-<<<<<<< HEAD
-        control: 'javascripts/control'
-=======
         control: 'javascripts/control',
         about: 'javascripts/about',
         work: 'javascripts/work'
->>>>>>> week06
     }
 });
 
 requirejs(['jquery'], function($) {
-
-<<<<<<< HEAD
-    requirejs(['bootstrap', 'control'], function(bootstrap, control) {
-=======
     requirejs(['bootstrap', 'about','control'], function(bootstrap,about,control) {
->>>>>>> week06
         control.init();
     });
 });
