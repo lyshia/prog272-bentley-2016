001273268e96d8ac32e03a2927daffbf34cb265a
lyshia <alyshiabentley@gmail.com>
2016-05-30 00:40:53 -0700

    add solar

---
+++ SolarVoyager/public/javascripts/renewables/renewables-by-index.js
@@ -0,0 +1,41 @@
+define(function() {
+    //Do setup work here
+    function getByIndex() {
+        console.log("get by index called");
+        var userInput = $('#renewableByIndexInput').val();
+        $.getJSON('/renewables/renewableByIndex/' + userInput, function(response) {
+                console.log(response);
+                $('#debug').html(JSON.stringify(response, null, 4));
+            })
+            .done(function() {
+                console.log("second success");
+            })
+            .fail(function(a, b, c) {
+                console.log('Error', a, b, c);
+                $('#debug').html('Error occured: ', a.status);
+            })
+            .always(function() {
+                console.log("complete");
+            });
+    }
+
+    var renewablesByIndex = {
+        color: "Show Renewables",
+        size: "by Index",
+        init: function() {
+            console.log(renewablesByIndex.color);
+            //var that = this;
+            $('#elf-view').load('/renewables/renewables-by-index-page', function() {
+                $('#display').html(renewablesByIndex.color + ' ' + renewablesByIndex.size);
+                $('#renewableByIndexInput').change(function() {
+                    getByIndex();
+                })
+                getByIndex();
+
+            });
+        }
+    };
+    return renewablesByIndex;
+
+
+});
